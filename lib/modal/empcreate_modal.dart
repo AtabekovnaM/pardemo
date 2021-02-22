5e6ab964e0a2d4d0e936240f05147cdb04e18d9b
Muslima <muslimabazarova2@gmail.com>
2021-02-22 19:01:35 +0600

    Initial commit

---
+++ lib/modal/empcreate_modal.dart
@@ -0,0 +1,19 @@
+import 'package:pardemo/modal/emp_modal.dart';
+
+class EmpCreate {
+  String status;
+  String message;
+  Employee data;
+
+  EmpCreate.fromJson(Map<String, dynamic> json)
+      : status = json['status'],
+        message = json['message'],
+        data = Employee.fromJson(json['data']);
+
+  Map<String, dynamic> toJson() => {
+    "status": status,
+    "message": message,
+    "data": data.toJson(),
+  };
+
+}
---
+++ lib/pages/detail_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:pardemo/modal/emp_modal.dart';
+import 'package:pardemo/modal/empone_model.dart';
+import 'package:pardemo/services/http_service.dart';
+
+class DetailPage extends StatefulWidget {
+
+  static final String id = "detail_page";
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+
+  Employee items;
+
+  void _apiEmpOne(int id) {
+    Network.GET(Network.API_EMP_ONE + id.toString(), Network.paramsEmpty()).then((response) => {
+      print(response),
+      _showResponse(response),
+    });
+  }
+
+  void _showResponse(String response) {
+    EmpOne empOne = Network.parseEmpOne(response);
+    setState(() {
+      items = empOne.data;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _apiEmpOne(1);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("Detail Page"),
+        ),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, i) {
+          return itemOfList(items);
+        },
+      ),
+    );
+  }
+
+  Widget itemOfList(Employee emp) {
+    return GestureDetector(
+      child: Center(
+        child: Container(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.only(bottom: 1),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(emp.employee_name+"("+emp.employee_age.toString()+")",style: TextStyle(color: Colors.black,fontSize: 20),),
+                SizedBox(height: 10,),
+                Text(emp.employee_salary.toString()+"\$",style: TextStyle(color: Colors.black,fontSize: 18),),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
